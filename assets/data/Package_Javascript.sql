5fbbb0d08cd66c53845ab06f487063319a2ce205
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2024-12-12 09:53:21 +0100

    added the SQL script

--- assets/data/Package_Javascript.sql
+++ assets/data/Package_Javascript.sql
@@ -3,21 +3,21 @@ CREATE DATABASE IF NOT EXISTS packages_js_db;
 USE packages_js_db;
 
 CREATE TABLE authors (
-  author_id int UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  author_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   name varchar(100) NOT NULL,
   email varchar(255) UNIQUE NOT NULL,
   registration_date date NOT NULL DEFAULT (CURRENT_DATE)
 );
 
 CREATE TABLE packages (
-  package_id int UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  package_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   name varchar(100) NOT NULL,
   description text,
   creation_date date NOT NULL DEFAULT (CURRENT_DATE)
 );
 
 CREATE TABLE versions (
-  version_id int UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  version_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   package_id int NOT NULL,
   version_number varchar(50) NOT NULL,
   release_date date NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE package_authors (
   author_id int NOT NULL,
   PRIMARY KEY (package_id, author_id)
 );
-
+ 
 CREATE INDEX package_authors_index_0 ON package_authors (package_id);
 
 CREATE INDEX package_authors_index_1 ON package_authors (author_id);
